87b35e43a806d2ee0b0171d9381b9022eeaf3d85
shubba04 <168166063+shubba04@users.noreply.github.com>
2025-01-17 08:13:56 +0100

    Add files via upload
    Assignment for SQL database

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Students (
+    ID INT,
+    Name VARCHAR(50),
+    MatriculationNumber VARCHAR(20),
+    Gender VARCHAR(7) 
+);
+INSERT INTO Students (ID, Name, MatriculationNumber, Gender) 
+VALUES (200480, 'danie ogbo', '20CJ00041', 'Male'),
+       (210018, 'chris micheal', '21CJ00012', 'Male'),
+       (210024, 'Monrow jess', '21CJ00033', 'Male'),
+       (210096, 'phil spenser', '21CJ00004', 'Male'),
+       (190019, 'Don ron', '19CJ00065', 'Male'),
+       (200002, 'Bukayo Saka', '20CJ00016', 'Male'),
+       (200008, 'saka peter', '20CJ00065', 'Male'),
+       (210015, 'Pep rotunda', '21CJ00058', 'Male'),
+       (200045, 'Michael finch', '20CJ00009', 'Female'),
+       (190059, 'Olivia Thomas', '19CJ00014', 'Female');
